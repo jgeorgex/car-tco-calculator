1d09ac8651509fba1f933b5a32cacb322e7cdc6a
Julian George <jules104@gmail.com>
2020-05-07 17:02:17 +0100

    Results sent to page by runCalculator on form submit

--- runCalculator.js
+++ runCalculator.js
@@ -1,14 +1,20 @@
 document.getElementById('form').addEventListener('submit', runCalculator);
 
 function runCalculator(e) {
+  e.preventDefault();
   const maintenance = parseInt(document.getElementById('maintenance').value);
   const tax = parseInt(document.getElementById('tax').value);
   const insurance = parseInt(document.getElementById('insurance').value);
-  const todaysValue = parseInt(document.getElementById('todays-value').value);
+  const value = parseInt(document.getElementById('todays-value').value);
   const futureValue = parseInt(document.getElementById('future-value').value);
   const fuel = parseInt(document.getElementById('fuel').value);
-
-  e.preventDefault();
+  const result = calculator(
+    maintenance,
+    tax,
+    insurance,
+    value,
+    futureValue,
+    fuel
+  );
+  document.getElementById('result').innerHTML = result;
 }
-
-export { maintenance, tax, insurance, todaysValue, futureValue, fuel };
