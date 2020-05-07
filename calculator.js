0ad44ddca390fcfb61b971654561007678da0ede
Julian George <jules104@gmail.com>
2020-05-07 11:33:41 +0100

    refactor calculator function revert back to not using event

--- calculator.js
+++ calculator.js
@@ -1,16 +1,7 @@
-document.getElementById('form').addEventListener('submit', calculator);
-
-function calculator(e) {
-  const maintenance = parseInt(document.getElementById('maintenance').value);
-  const tax = parseInt(document.getElementById('tax').value);
-  const insurance = parseInt(document.getElementById('insurance').value);
-  const todaysValue = parseInt(document.getElementById('todays-value').value);
-  const futureValue = parseInt(document.getElementById('future-value').value);
-  const fuel = parseInt(document.getElementById('fuel').value);
-  const tco =
-    (maintenance + tax + insurance + todaysValue - futureValue + fuel) / 12;
-  console.log(tco);
-  e.preventDefault();
+function calculator(maintenance, tax, insurance, value, futureValue, fuel) {
+  return Math.round(
+    (maintenance + tax + insurance + fuel + value - futureValue) / 12
+  );
 }
 
-// module.exports = calculator;
+module.exports = calculator;
