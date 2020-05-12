12e82f3d45bc103402da4d43b1a547cda8bf83dd
Julian George <jules104@gmail.com>
2020-05-12 10:46:23 +0100

    Add visit homepage test

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -3,3 +3,9 @@ describe('My First Test', () => {
     expect(true).to.equal(true);
   });
 });
+
+describe('Homepage tests', () => {
+  it('Visits homepage', () => {
+    cy.visit('http://127.0.0.1:5500/index.html');
+  });
+});
