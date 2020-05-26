c04257e0b1c10304bee21521a50f2f66d7f83269
Julian George <jules104@gmail.com>
2020-05-26 17:37:23 +0100

    Correct typo in test

--- jest/calculator.test.js
+++ jest/calculator.test.js
@@ -8,18 +8,18 @@ test('calulates monthly cost with all input except car values', () => {
   expect(calculator(12, 12, 12, 0, 0, 12)).toBe(4);
 });
 
-test('calulates monthly cost with all inputs car has 0 deprecation', () => {
+test('calulates monthly cost with all inputs car has 0 depreciation', () => {
   expect(calculator(12, 12, 12, 100, 100, 12)).toBe(4);
 });
 
-test('calulates monthly cost with all inputs car has -12 deprecation', () => {
+test('calulates monthly cost with all inputs car has -12 depreciation', () => {
   expect(calculator(12, 12, 12, 100, 88, 12)).toBe(5);
 });
 
-test('calulates monthly cost with all inputs car has +12 deprecation', () => {
+test('calulates monthly cost with all inputs car has +12 depreciation', () => {
   expect(calculator(12, 12, 12, 100, 112, 12)).toBe(3);
 });
 
-test('calulates monthly cost with all inputs car has -5000 deprecation', () => {
+test('calulates monthly cost with all inputs car has -5000 depreciation', () => {
   expect(calculator(1500, 350, 700, 50000, 45000, 1000)).toBe(713);
 });
